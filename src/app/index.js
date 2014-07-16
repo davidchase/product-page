4c04a963b2100824258f778524111f363adab7c3
David Annopolsky <dannopolsky@freepeople.com>
2014-07-15 23:25:49 -0400

    using lodash-node for requiring instead of prebuilt

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*  
+ *  Can be done with
+ *  require('angularjs/angular');
+ *  which exposes angular to the window.
+ *  This is a current stable build from
+ *  angular hosted as a bower package.
+ *
+ */
 var angular = require('angular');
 var productImages = require('./product-images');
 var productInfo = require('./product-info');
